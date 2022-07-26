617f88af3975196d5afc13f77377fde77f369437
jgsch <jean.gschwind@protonmail.com>
2022-07-26 16:55:22 +0200

    symlink setup script

---
+++ install.sh
@@ -0,0 +1,31 @@
+NAME=$1
+EMAIL=$2
+
+# oh-my-zsh theme
+ln -sf $PWD/oh-my-zsh-themes/custom-theme-01.zsh-theme $HOME/.oh-my-zsh/themes/custom-theme-01.zsh-theme
+ln -sf $PWD/oh-my-zsh-themes/custom-theme-02.zsh-theme $HOME/.oh-my-zsh/themes/custom-theme-02.zsh-theme
+
+# lsd
+mkdir -p $HOME/.config/lsd
+ln -sf $PWD/lsd/config.yaml $HOME/.config/lsd/config.yaml
+
+
+# Tmux
+ln -sf $PWD/tmux/tmux.conf $HOME/.tmux.conf
+
+# Alacritty
+mkdir -p $HOME/.config/alacritty
+ln -sf $PWD/alacritty/alacritty.yml $HOME/.config/alacritty/alacritty.yml
+
+# Helix
+mkdir -p $HOME/.config/helix
+ln -sf $PWD/helix/config.toml $HOME/.config/helix/config.toml
+ln -sf $PWD/helix/languages.toml $HOME/.config/helix/languages.toml
+
+# Github alias
+git config --global user.email $EMAIL
+git config --global user.name  $NAME
+git config --global core.editor "hx"
+git config --global alias.car 'commit --amend --no-edit'
+git config --global alias.unstage 'reset'
+git config --global alias.uncommit 'reset --soft HEAD^'

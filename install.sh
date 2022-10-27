3a606a5e977fd653b22db0b8c33cb13f1a51dade
jgsch <jean.gschwind@protonmail.com>
2022-10-27 15:02:58 +0200

    add ohmyzsh install in script

--- install.sh
+++ install.sh
@@ -1,6 +1,11 @@
 NAME=$1
 EMAIL=$2
 
+# install oh-my-zsh if needed
+if [[ ! -d "${HOME}/.oh-my-zsh" ]]; then
+  sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+fi
+
 # oh-my-zsh theme
 ln -sf $PWD/oh-my-zsh-themes/custom-theme-01.zsh-theme $HOME/.oh-my-zsh/themes/custom-theme-01.zsh-theme
 ln -sf $PWD/oh-my-zsh-themes/custom-theme-02.zsh-theme $HOME/.oh-my-zsh/themes/custom-theme-02.zsh-theme
